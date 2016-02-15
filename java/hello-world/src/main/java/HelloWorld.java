c3026f17e2f932170ba6b7e25030d3a79640d78a
Fernando Almeida <fernando@fernandoalmeida.net>
2016-02-15 10:28:29 -0300

    [java] Hello World!

---
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String hello(String name) {
+	if (name == null || name.length() == 0) {
+	    name = "World";
+	}
+
+	return String.format("Hello, %s!", name);
+    }
+}
