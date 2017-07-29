04fdb017ac617b845a4c96d890e16c56fb47cdda
Fernando Almeida <fernando@fernandoalmeida.net>
2017-07-29 04:29:09 -0300

    [exercism][bash] Hamming Distance

--- exercism.io/bash/hamming/hamming.sh
+++ exercism.io/bash/hamming/hamming.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -e
 
 if [[ $# != 2 ]] ; then
-    echo "Usage: ./hamming.sh <string1> <string2>"
-    exit 1
+  echo "Usage: ./hamming.sh <string1> <string2>" && exit 1
 elif [[ ${#1} != ${#2} ]] ; then
-    echo "The two strands must have the same length."
-    exit 1
-else
-    declare -i distance=0
-    for (( i=0 ; i < ${#1} ; i++ )) ; do
-	if [[ ${1:i:1} != ${2:i:1} ]] ; then
-	    distance+=1
-	fi
-    done
-    echo $distance
+  echo "The two strands must have the same length." && exit 1
 fi
+
+declare -i distance=0
+
+for (( i=0 ; i < ${#1} ; i++ )) ; do
+  if [[ ${1:i:1} != ${2:i:1} ]] ; then distance+=1; fi
+done
+
+echo $distance
