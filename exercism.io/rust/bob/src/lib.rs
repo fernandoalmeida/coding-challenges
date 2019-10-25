187de238002365670d8718ed474fbdd4072e5bb8
Fernando Almeida <fernando@fernandoalmeida.net>
2019-10-25 09:04:38 -0300

    [exercism][rust] Clarify pattern matching in "Bob" solution

--- exercism.io/rust/bob/src/lib.rs
+++ exercism.io/rust/bob/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn reply(message: &str) -> &str {
     let empty:bool = message.trim() == "";
     let has_word:bool = message.chars().any(|c| c.is_alphabetic());
-    let shout:bool = message.to_uppercase() == message;
+    let yell:bool = message.to_uppercase() == message;
     let question:bool = message.trim().ends_with("?");
 
-    match (empty, has_word, shout, question) {
-        (true, _, _, _) => "Fine. Be that way!",
-        (_, true, true, true) => "Calm down, I know what I'm doing!",
-        (_, true, true, false) => "Whoa, chill out!",
-        (_, _, _, true) => "Sure.",
-        (_, _, _, _) => "Whatever."
+    match true {
+        _ if empty => "Fine. Be that way!",
+        _ if has_word && yell && question => "Calm down, I know what I'm doing!",
+        _ if has_word && yell => "Whoa, chill out!",
+        _ if question => "Sure.",
+        _ => "Whatever."
     }
 }
